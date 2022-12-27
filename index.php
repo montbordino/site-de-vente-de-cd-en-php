22f76eb45f1b856795c1f76f82e2197d6ecdad98
tom montbord <montbordino@gmail.com>
2022-12-28 00:47:58 +0100

    première version de l'index.php avec un header

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo "ca marche";
+// cette page à pour but d'accueil l'utilisateur et de lui présenter le site, il n'a pas besoin de se connecter pour y accéder
+
+include_once('src/php/header.php');
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="public/scss/index.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="sous-header">
+        <div class="bg">
+            <img src="public/images/bg_index.jpeg" alt="image floue de cds">
+        </div>
+        <div class="contenu">
+            <h1>Bienvenue</h1>
+            <br> <br>
+            <p>Si vous cherchez des cd vous êtes au bon endroit</p>
+        </div>
+    </div>
+</body>
+</html>
