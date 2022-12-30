1f12cf694eedeca46720bc038f35c373ffe0e35b
tom montbord <montbordino@gmail.com>
2022-12-30 21:09:47 +0100

    le lien permet maintenant d'accéder aux pages de connexion/déconnexion depuis toutes les pages (sauf header.php car inaccessible pour l'utilisateur)

--- src/php/header.php
+++ src/php/header.php
@@ -9,10 +9,22 @@
     <div class="login-info">
         <?php
         session_start();
+
+        $emplacement = debug_backtrace()[0]['file']; // path absolut du fichier appelant header.php
+        $dernier_dir = basename(dirname($emplacement)); // dossier contenant le fichier appelant header.php
+        if ($dernier_dir == "php"){
+            $lien = "";
+        }
+        else { // le fichier est index.php
+            $lien = "src/php/";
+        }
+
         if (isset($_SESSION['email'])) {
-            echo "<a href='logout.php'>Connecté avec " . $_SESSION['email'] . ".</a>";
+            $lien .= 'deconnexion.php';
+            echo "<a href=$lien>Connecté avec " . $_SESSION['email'] . ".</a>";
         } else {
-            echo '<a href="connexion.php">Connexion</a>';
+            $lien .= 'connexion.php';
+            echo "<a href=$lien>connexion</a>";
         }
         ?>
     </div>
