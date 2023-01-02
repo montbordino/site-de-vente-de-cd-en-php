7958f1d70198b094be428fee8a1aeddbc9929fb2
tom montbord <montbordino@gmail.com>
2023-01-02 13:05:34 +0100

    ajout de la structure des produits et des filtres

--- index.php
+++ index.php
@@ -24,8 +24,65 @@ include_once('src/php/header.php');
             <br> <br>
             <p>Si vous cherchez des cd vous êtes au bon endroit</p>
             <br> <br>
-            <a href="shop.php"><button> commencer mes achats </button></a>
+            <a href="src/php/shop.php"><button> commencer mes achats </button></a>
         </div>
     </div>
+
+    <main>
+        <aside>
+            <h2>Filtres</h2>
+            <form>
+                <label for="genre">Genre</label>
+                <select name="genre" id="genre">
+                    <option value="rock">Rock</option>
+                    <option value="pop">Pop</option>
+                    <option value="rap">Rap</option>
+                    <option value="classique">Classique</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="metal">Metal</option>
+                    <option value="reggae">Reggae</option>
+                    <option value="electro">Electro</option>
+                    <option value="variété">Variété</option>
+                </select>
+                <br> <br>
+                <label for="ordre">Trier par</label>
+                <select name="ordre" id="ordre">
+                    <option value="prix">Prix croissant</option>
+                    <option value="prix">Prix décroissant</option>
+                </select>
+                <br> <br>
+
+                <label for="prix">Prix</label>
+                <select name="prix" id="prix">
+                    <option value="0-20">0-20€</option>
+                    <option value="20-50">20-50€</option>
+                    <option value="50-100">50-100€</option>
+                    <option value="+100">+100</option>
+                </select>
+                <br> <br>
+
+                <label for="artiste">Artiste</label>
+                <input type="text" name="artiste" id="artiste">
+                <br> <br>
+                <input type="submit" value="Appliquer">
+            </form>
+        </aside>
+        <div id="content">
+            <input type="text" placeholder="cherchez un titre">
+
+            <section id="produits">
+                <!-- exemple de la structure d'un produit
+                <div class="article">
+                    <img src="//unsplash.it/300/300">                    <h3>Titre</h3>
+                    <p>Artiste</p>
+                    <p>Prix</p>
+                    <button>Ajouter au panier</button>
+                </div>
+                -->
+
+
+            </section>
+        </div>
+    </main>
 </body>
 </html>
