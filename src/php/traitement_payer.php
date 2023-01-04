fdb23252fefe8696deba0cbab193c447910f46fb
tom montbord <montbordino@gmail.com>
2023-01-04 15:02:30 +0100

    supprime le pannier si l'achat est validé

--- src/php/traitement_payer.php
+++ src/php/traitement_payer.php
@@ -1,4 +1,10 @@
 <?php
+    require_once("../BD/ouverture_bd.php");
+    if(session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+
+
     if (isset($_POST["submit"])) {
         $n_carte = $_POST["carte"];
         $date = $_POST["date"];
@@ -10,8 +16,16 @@
             if($date > date("Y-m-d", strtotime("+3 months"))){
                 // verification si le cryptogramme est valide
                 if(strlen($crypto) == 3){
-                    mail($_POST["email"], "Confirmation de commande", "Votre commande chez CD SHOP a bien été prise en compte. Vous recevrez un mail de confirmation de la part de notre partenaire de livraison.");
-                    header("Location: resultat_payer.php");
+                    if(!empty($bd)) {
+                        // suppression des produits du panier
+                        $stmt = $bd->prepare("DELETE FROM panier WHERE ID_CLIENT = :id_client");
+                        $stmt->execute(['id_client' => $_SESSION['id']]);
+                        $stmt->closeCursor();
+
+                        mail($_POST["email"], "Confirmation de commande", "Votre commande chez CD SHOP a bien été prise en compte. Vous recevrez un mail de confirmation de la part de notre partenaire de livraison.");
+                        header("Location: resultat_payer.php");
+                    }
+
                 }
                 else{
                     $erreur = "Cryptogramme invalide";
